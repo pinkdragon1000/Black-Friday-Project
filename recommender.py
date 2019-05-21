96069539e217d9e0db3ffd7c4e200192248289bf
shindig7 <jal457@drexel.edu>
2019-05-21 15:45:47 -0400

    Formatting

--- recommender.py
+++ recommender.py
@@ -5,8 +5,10 @@ import logging
 FORMAT = "[%(asctime)s] - [%(levelname)s] - [%(funcName)s] - %(message)s"
 logging.basicConfig(level=20, format=FORMAT)
 
+
 def get_similarity(vec1, vec2):
     return 1 - cosine(vec1, vec2)
+
 
 def collect_pairs(vectors, limit=1000):
     logging.info("Collecting pairs, limit={}".format(limit))
@@ -40,21 +42,25 @@ def comparisons(comp_tuple):
 
 def get_closest(user_id, vectors, n=5):
     user_vec = vectors[int(user_id)]
-    candidates = [((user_id, user_vec), (k, v)) for k,v in vectors.items() if user_id != k]
-    candidates = sorted(list(map(comparisons, candidates)), key=lambda k: k[2], reverse=True)
+    candidates = [
+        ((user_id, user_vec), (k, v)) for k, v in vectors.items() if user_id != k
+    ]
+    candidates = sorted(
+        list(map(comparisons, candidates)), key=lambda k: k[2], reverse=True
+    )
     return candidates[:n]
-
 
 
 def main():
     from pprint import pprint
+
     purchase_df, user_df = get_data()
     vectors = get_vectors(user_df)
     pprint(get_closest(1000001, vectors, n=10))
-    #print(vectors[1000001])
-    #sim = compare_all(vectors)
-    #sim.sort(key=lambda k: k[2], reverse=True)
-    #print(sim)
+    # print(vectors[1000001])
+    # sim = compare_all(vectors)
+    # sim.sort(key=lambda k: k[2], reverse=True)
+    # print(sim)
 
 
 if __name__ == "__main__":
